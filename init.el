1813ed767f1c21418d0f1fb56679381e1872b7c2
William Ayd <will_ayd@innobi.io>
2022-11-01 13:03:18 -0700

    lsp integration

--- init.el
+++ init.el
@@ -1,24 +1,14 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
  '(package-selected-packages
- '(package-selected-packages
-   '(rust-mode lsp-mode web-mode-edit-element add-node-modules-path prettier emmet-mode prettier-js web-mode typescript-mode cmake-mode which-key use-package doom-themes)))
-(setq auto-save-file-name-transforms
-      `((".*" "~/.emacs-saves/" t)))
-(setq backup-directory-alist `(("." . "~/.emacs-saves")))
-
-(require 'use-package)
-
-;; Google C style guide
-(add-hook 'c++-mode-hook 'google-set-c-style)
-(add-hook 'c++-mode-hook 'google-make-newline-indent)
-
+   '(conda transpose-frame lsp-pyright ripgrep projectile exec-path-from-shell company lsp-ui rust-mode lsp-mode web-mode-edit-element add-node-modules-path prettier emmet-mode prettier-js web-mode typescript-mode cmake-mode which-key use-package doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,8 +16,16 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; all below is heavily inspired by
-;; https://github.com/daviwil/emacs-from-scratch/blob/master/init.el
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(setq auto-save-file-name-transforms
+      `((".*" "~/.emacs-saves/" t)))
+(setq backup-directory-alist `(("." . "~/.emacs-saves")))
+
+(eval-when-compile
+  (require 'use-package))
+
 (setq inhibit-startup-message t)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
@@ -39,50 +37,53 @@
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-(set-face-attribute 'default nil :font "Fira Code Retina")
-;; https://github.com/mickeynp/ligature.el
-;; https://github.com/tonsky/FiraCode/wiki/Emacs-instructions
-(use-package ligature
-  :load-path "/home/willayd/clones/ligature.el"
-  :config
-  (ligature-set-ligatures 't '("www"))
-  (ligature-set-ligatures 'prog-mode '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\" "{-" "::"
-                                     ":::" ":=" "!!" "!=" "!==" "-}" "----" "-->" "->" "->>"
-                                     "-<" "-<<" "-~" "#{" "#[" "##" "###" "####" "#(" "#?" "#_"
-                                     "#_(" ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*" "/**"
-                                     "/=" "/==" "/>" "//" "///" "&&" "||" "||=" "|=" "|>" "^=" "$>"
-                                     "++" "+++" "+>" "=:=" "==" "===" "==>" "=>" "=>>" "<="
-                                     "=<<" "=/=" ">-" ">=" ">=>" ">>" ">>-" ">>=" ">>>" "<*"
-                                     "<*>" "<|" "<|>" "<$" "<$>" "<!--" "<-" "<--" "<->" "<+"
-                                     "<+>" "<=" "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<"
-                                     "<~" "<~~" "</" "</>" "~@" "~-" "~>" "~~" "~~>" "%%"))
-  
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
-
 (use-package doom-themes
   :init (load-theme 'doom-palenight t))  ;; also like doom-gruvbox
 
-(use-package which-key
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay .4))
-
-;; Typescript development
+(use-package which-key)
 (use-package js-mode
-  :mode "\\.[jt]sx?\\'")
+  :mode "\\.[jt]sx?\\'"
+  :config
+  (setq js-indent-level 2))
 
 (eval-after-load 'js-mode
   (progn
     (add-hook 'js-mode-hook #'add-node-modules-path)
-    (add-hook 'js-mode-hook #'web-mode-hook
     (add-hook 'js-mode-hook #'emmet-mode)
-    (setq js-indent-level 2)))
-                 
+    (add-hook 'js-mode-hook #'prettier-js-mode)))
+
 (use-package rust-mode
   :init (add-to-list 'exec-path "~/.cargo/bin")
   :hook prettify-symbols
   :config
   (setq indent-tabs-mode nil)
   (setq rust-format-on-save t))
+
+(use-package projectile
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/clones")
+    (setq projectile-project-search-path '("~/clones"))))
+
+(use-package lsp
+  :commands (lsp lsp-deferred)
+  :hook (c++-mode))
+
+(add-to-list 'auto-mode-alist '("\\.py*" . python-mode))
+(add-to-list 'auto-mode-alist '("\\.pxi*" . python-mode))
+(require 'conda)
+;; if you want auto-activation (see below for details), include:
+(conda-env-autoactivate-mode t)
+;; if you want to automatically activate a conda environment on the opening of a file:
+
+(defun my-conda-hook()
+  (when (and (stringp buffer-file-name)
+             (string-match "\\.py*\\'" buffer-file-name))
+    (conda-env-activate-for-buffer)))
+
+(add-hook 'find-file-hook 'my-conda-hook)
+
+(use-package lsp-pyright
+  :hook (python-mode . (lambda ()
+                         (lsp-deferred))))
