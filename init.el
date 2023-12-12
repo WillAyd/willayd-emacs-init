fb7d80ae8ab3d92ddf835325af693d28f990e6d7
Will Ayd <william.ayd@icloud.com>
2023-12-12 15:38:16 -0500

    replace lsp with eglot; simplifcations

--- init.el
+++ init.el
@@ -6,9 +6,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("3bf80e2e3f2c3dd0295be05d2e44815e7d810c2b1cb3fb8fe1a7bc50634f5f0e" "b579486e2ebd9d36460ea4b976fa37c845f8158506d85626b95b4c9a37baafee" "54047744cbb0de08c5591f3fe4a819cc6d550e312b490c588bce11d041d61cc1" "712166475816156b9faeae7b42953296fa0967b061598470ae7ab0d26bcf2c41" "1975855abd3275efe27c49cac8386c3912733ec106cbadf969e371c625941002" "891cf99ff093c965fd7a5dd9a0122783ae678444afec77fc1bd6350d8dc45a3e" "d393d418cac62d024af05f25ed2a0cd92115417684e9fd61d2f0a7dcb1dcdc2b" "babfd0d32561d4afb6ba0f0ccfe9fd15d1c209d16b3ac796c27f434fec60dbfe" "5331f36284c063183f974fa586b6af84b1a7200dbde5802f295131f129bd1e3f" "0f5867c6d5c9f8c98de70cd5be78ff6799b87166e78e3ecd23617108dabb0cab" "bf08d37ebbad67f0748f216ed2af6ec2d4387fc225be245c43ab64844dea458b" "3994b1c5c1f412c025bb71b2df5973a421c257afee5278684639c205d79d93ee" "b52b2f39598253df2d54edeaa8516bd7b5346e5c0fdf04076b14d9b2ad03a657" "985d398b7391c6d653e94c7bc0255d492a13c2fe80d2b8c5d0f40e828ef7b561" "3074fda75f35f990d112fb75681729a74b6c7f15d3e5dfcf80313abb4cd39ed8" "137c792e21ec0c02f71c23d3342cbad5a39c5468f032798ac3e2004e0d254930" "bb613853010a8b4c4d18ed8adcfc233990c88fa4ef266ccd9a54a635848e6d55" "1e2d250d8d45cfb34f2ddf7475e5d1784835f2f52c78ddd4d91e6e667a2d55a8" "2bd8ad41d7c70771c57f8e446be6760e05bc1d1f111d2d984607f644e904b4ca" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(lsp-treemacs command-log-mode lsp-pyright clang-format ivy magit conda transpose-frame ripgrep projectile exec-path-from-shell company lsp-ui rust-mode lsp-mode web-mode-edit-element add-node-modules-path prettier emmet-mode prettier-js web-mode typescript-mode cmake-mode which-key use-package doom-themes)))
+   '(ivy magit transpose-frame ripgrep projectile exec-path-from-shell company rust-mode web-mode-edit-element web-mode typescript-mode cmake-mode which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -32,32 +34,16 @@
 (tooltip-mode -1)
 (menu-bar-mode -1)
 
-(use-package command-log-mode
-  :commands command-log-mode
-  :hook prog-mode)
-
 (setq-default indent-tabs-mode nil)
 (setq visible-bell t)
 (column-number-mode)
 (global-display-line-numbers-mode t)
 
-(use-package doom-themes
-  :init (load-theme 'doom-palenight t))  ;; also like doom-gruvbox
+(load-theme 'popOS t)
 
 (use-package which-key
   :config
   (which-key-mode))
-
-(use-package js-mode
-  :mode "\\.[jt]sx?\\'"
-  :config
-  (setq js-indent-level 2))
-
-(eval-after-load 'js-mode
-  (progn
-    (add-hook 'js-mode-hook #'add-node-modules-path)
-    (add-hook 'js-mode-hook #'emmet-mode)
-    (add-hook 'js-mode-hook #'prettier-js-mode)))
 
 (use-package rust-mode
   :init (add-to-list 'exec-path "~/.cargo/bin")
@@ -72,12 +58,8 @@
   (ivy-mode 1))
 
 (use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
-              ("<tab>" . company-complete-selection))
-  (:map lsp-mode-map
-        ("<tab>" . company-indent-or-complete-common)))
+              ("<tab>" . company-complete-selection)))
 
 (use-package projectile
   :diminish projectile-mode
@@ -90,40 +72,9 @@
     (setq projectile-project-search-path '("~/clones")))
   (setq projectile-switch-project-action #'projectile-dired))
 
-(use-package lsp
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l")  
-  (setq lsp-clients-clangd-args '("--header-insertion-decorators=0" "--limit-references=0" "--log=verbose"))
-  :hook (c++-mode c-mode))
-
 (add-to-list 'auto-mode-alist '("\\.py*" . python-mode))
 (add-to-list 'auto-mode-alist '("\\.pxi*" . python-mode))
-(require 'conda)
-;; if you want auto-activation (see below for details), include:
-;;(conda-env-autoactivate-mode t)
-;; if you want to automatically activate a conda environment on the opening of a file:
-
-(defun my-conda-hook()
-  (when (and (and(stringp buffer-file-name)
-                 (string-match "\\.py*\\'" buffer-file-name))
-             (not (bound-and-true-p conda-env-current-name)))
-    (conda-env-activate-for-buffer)))
-
-(add-hook 'find-file-hook 'my-conda-hook)
-
-(defun my-lsp-hook()
-     (when (or (string-match "\\.pyx\\'" buffer-file-name)
-               (string-match "\\.pxd\\'" buffer-file-name))
-       (setq lsp-diagnostics-provider :none))
-     (lsp-deferred))
-
-(use-package lsp-pyright
-  :hook (python-mode . my-lsp-hook))
 
 (defun my-cpp-setup ()
   (c-set-offset 'innamespace [0]))
 (add-hook 'c++-mode-hook 'my-cpp-setup)
-
-;;(use-package clang-format
-;;  :hook c++-mode)
